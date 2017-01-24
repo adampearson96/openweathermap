b4be0a218061c72b8519a74e7ea70a736ec6fc12
Adam Pearson <adampearson@adams-mbp.home>
2017-01-24 17:48:27 +0000

    logging all wanted values from JSON object separately

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,39 @@ function loadWeather() {
 	weatherCall.onreadystatechange = function() {
     if( weatherCall.readyState == XMLHttpRequest.DONE ) {
 	 		if( weatherCall.status == 200 ) {
-        document.getElementById( "weather_results" ).innerHTML = weatherCall.responseText; // write out json to screen
+        obj = weatherCall.responseText;
+        data = JSON.parse(obj);
+        document.getElementById( "weather_results" ).innerHTML = obj; // write out json to screen
+        // Get the icon code for the weather type and store it
+        var weatherIcon = data.weather[0].icon;
+        // Create variable to use to get the icon url
+        var imageString = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+        // Time data is captured
+        console.log(data.dt);
+        // Weather (type, brief description, icon)
+        console.log(data.weather[0].main);
+        console.log(data.weather[0].description);
+        console.log(imageString);
+        // Temperature, Pressure, Humidity
+        console.log(data.main.temp);
+        console.log(data.main.pressure);
+        console.log(data.main.humidity);
+        // Wind Speed, Direction
+        console.log(data.wind.speed);
+        console.log(data.wind.deg);
+        // Cloudiness (%)
+        console.log(data.clouds.all);
+        // rainfall (volume in last 3 hours)
+        console.log(data.rain);
+        // snowfall (volume in last 3 hours)
+        console.log(data.snow);
+        // Sunset, Sunrise, 
+        console.log(data.sys.sunrise);
+        console.log(data.sys.sunset);
+        // country, city
+        console.log(data.sys.country);
+        console.log(data.name);
+
       }
     	else if( weatherCall.status == 400 ) {
         alert( 'There was an error 400' );
@@ -24,4 +56,5 @@ function loadWeather() {
   weatherCall.open( "GET", apiString, true ) 
   // Execute call
 	weatherCall.send();
+  var weatherResults = weatherCall.responseText;
 }

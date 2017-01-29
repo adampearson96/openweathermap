5eff1131894126302b9570dde8a3373ebe88dec9
Adam Pearson <adampearson@adams-mbp.home>
2017-01-29 16:28:41 +0000

    adding html layout and styling for displaying results and creating each result as a variable for reuse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,68 @@
+function hideFields() {
+  document.getElementById( "primary_weather_results" ).style.display = "none";
+  document.getElementById( "secondary_weather_results" ).style.display = "none";
+  document.getElementById( "error_message" ).style.display = "none";
+}
 function loadWeather() {
   var citySearch = document.getElementById('cityName').value;
   var apiKey = "af54bfdccb0b7152a2b0fe992444c7f5";
-  var apiString = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&APPID=" + apiKey;
-
+  var apiString = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=metric" + "&APPID=" + apiKey;
 	// new variable to set up the request
 	var weatherCall = new XMLHttpRequest(); 
-
 	// Set up response options for what should happen once the response has sent
 	weatherCall.onreadystatechange = function() {
     if( weatherCall.readyState == XMLHttpRequest.DONE ) {
 	 		if( weatherCall.status == 200 ) {
         document.getElementById( "error_message" ).style.display = "none";
+        document.getElementById( "primary_weather_results" ).style.display = "block";
+        document.getElementById( "secondary_weather_results" ).style.display = "block";
+        // Get response from API call
         obj = weatherCall.responseText;
+        // Parse JSON
         data = JSON.parse(obj);
-        // Get the icon code for the weather type and store it
-        var weatherIcon = data.weather[0].icon;
-        // Create variable to use to get the icon url
-        var imageString = '<img src="http://openweathermap.org/img/w/' + weatherIcon + '.png" />';
-        // Time data is captured
-        console.log(data.dt);
-        // Weather (type, brief description, icon)
-        document.getElementById( "weather_type" ).innerHTML = data.weather[0].main;
-        document.getElementById( "weather_description" ).innerHTML = data.weather[0].description;
-        document.getElementById( "weather_image" ).innerHTML = imageString;
-        // Temperature, Pressure, Humidity
-        console.log(data.main.temp);
-        console.log(data.main.pressure);
-        console.log(data.main.humidity);
-        // Wind Speed, Direction
-        console.log(data.wind.speed);
-        console.log(data.wind.deg);
-        // Cloudiness (%)
-        console.log(data.clouds.all);
-        // rainfall (volume in last 3 hours)
-        console.log(data.rain);
-        // snowfall (volume in last 3 hours)
-        console.log(data.snow);
-        // Sunset, Sunrise, 
-        console.log(data.sys.sunrise);
-        console.log(data.sys.sunset);
-        // country, city
-        console.log(data.sys.country);
-        console.log(data.name);
-
+        // Create Weather Variables
+        var weatherTime = new Date( data.dt * 1000 ); // Time weather data was taken
+        var weatherType = data.weather[0].main; // Weather Type (e.g. Sunny)
+        var weatherDescription = data.weather[0].description; // Weather Description (e.g. A few clouds)
+        var weatherIcon = '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png" />'; // Weather Icon
+        var weatherTemperature = data.main.temp + "&#8451"; // Temperature (e.g. 2.1, Celcius)
+        var weatherPressure = data.main.pressure; // Atmospheric Pressure (hPa)
+        var weatherHumidity = data.main.humidity; // Humidity (%)
+        var weatherWindSpeed = data.wind.speed; // Wind Speed (Defaults to meters/second)
+        var weatherWindDirection = data.wind.deg; // Wind Direction (degrees, meteorological)
+        var weatherCloudiness = data.clouds.all; // Cloudiness (%)
+        var weatherRainfall = data.rain; // Rain volume in the last 3 hours
+        var weatherSnowfall = data.snow; // Snow volume in the last 3 hours
+        var weatherSunrise = new Date( data.sys.sunrise * 1000 ); // Sunrise (Unix, UTC)
+        var weatherSunset = new Date( data.sys.sunset * 1000 ); // Sunset (Unix, UTC)
+        var city_and_country = data.name + ", " + data.sys.country; // City Name, Country Code (e.g. Leeds, GB)
+        document.getElementById( "weather_time" ).innerHTML = weatherTime;
+        document.getElementById( "weather_icon" ).innerHTML = weatherIcon;
+        document.getElementById( "weather_temperature" ).innerHTML = weatherTemperature;
+        document.getElementById( "weather_type" ).innerHTML = weatherType;
+        document.getElementById( "weather_city" ).innerHTML = city_and_country;
+        document.getElementById( "weather_description" ).innerHTML = weatherDescription;
+        document.getElementById( "weather_pressure" ).innerHTML = weatherPressure;
+        document.getElementById( "weather_humidity" ).innerHTML = weatherHumidity;
+        document.getElementById( "weather_wind_speed" ).innerHTML = weatherWindSpeed;
+        document.getElementById( "weather_wind_direction" ).innerHTML = weatherWindDirection;
+        document.getElementById( "weather_cloudiness" ).innerHTML = weatherCloudiness;
+        document.getElementById( "weather_rainfall" ).innerHTML = weatherRainfall;
+        document.getElementById( "weather_snowfall" ).innerHTML = weatherSnowfall;
+        document.getElementById( "weather_sunrise" ).innerHTML = weatherSunrise;
+        document.getElementById( "weather_sunset" ).innerHTML = weatherSunset;  
       }
     	else if( weatherCall.status == 400 ) {
-        alert( 'There was an error 400' );
         document.getElementById( "error_message" ).innerHTML = "Uh oh. Something has gone wrong on our end. Please try again later, or alternatively contact a system administrator.";
         document.getElementById( "error_message" ).style.display = "block";
+        document.getElementById( "primary_weather_results" ).style.display = "none";
+        document.getElementById( "secondary_weather_results" ).style.display = "none";
       }
     	else {
       	document.getElementById( "error_message" ).innerHTML = "Oops, we were unable to find the weather for that region. Please enter a new city name or post code.";
         document.getElementById( "error_message" ).style.display = "block";
+        document.getElementById( "primary_weather_results" ).style.display = "none";
+        document.getElementById( "secondary_weather_results" ).style.display = "none";
       }
     }
   };
